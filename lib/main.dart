0baca63ac493df05a548fce0be83caee260bc9c8
GeraldGat <gerald.gatouillat@gmail.com>
2026-07-07 16:35:44 +0200

    Add FULLSCREEN env variable to start in fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutterbooth/screens/home_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutterbooth/providers/config_provider.dart';
 import 'package:flutterbooth/widgets/fb_keyboard_listener.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  final startFullscreen = Platform.environment.containsKey('FULLSCREEN');
+
+  await windowManager.waitUntilReadyToShow(
+    const WindowOptions(size: Size(1280, 720), center: true),
+    () async {
+      if (startFullscreen) {
+        await windowManager.setFullScreen(true);
+      }
+      await windowManager.show();
+      await windowManager.focus();
+    },
+  );
+
   runApp(const ProviderScope(child: App()));
 }
 
